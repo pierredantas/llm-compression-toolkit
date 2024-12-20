ff92ccabea70a52248365b279cc8fcb363c83492
pierredantas <pierre.dantas@gmail.com>
2024-12-20 18:20:24 +0000

    Delete quantization directory

--- quantization/__init__.py
+++
@@ -1,5 +0,0 @@
-# quantization/__init__.py
-from .quantizer import quantize_model
-from .utils import count_parameters, calculate_model_size, calculate_reduction_rate
-
-__all__ = ['quantize_model', 'count_parameters', 'calculate_model_size', 'calculate_reduction_rate']
