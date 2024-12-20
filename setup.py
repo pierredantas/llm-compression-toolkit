e60083235c75d5c270b59d88d012aec0c393dd19
pierredantas <pierre.dantas@gmail.com>
2024-12-20 18:13:25 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+# setup.py
 from setuptools import setup, find_packages
 
 setup(
@@ -5,12 +6,12 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
-        "torch>=1.8.0",
+        "torch>=1.9.0",
         "transformers>=4.0.0",
     ],
     author="Pierre Dantas",
     author_email="pierre.dantas@gmail.com",
-    description="A toolkit for compressing and quantizing language models",
+    description="A toolkit for LLM compression and quantization",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/pierredantas/llm-compression-toolkit",
