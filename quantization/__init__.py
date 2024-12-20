83bcd8be5e52f8d64dc2bd5378b6e26963f5ac1d
pierredantas <pierre.dantas@gmail.com>
2024-12-20 18:14:56 +0000

    Update __init__.py

--- quantization/__init__.py
+++ quantization/__init__.py
@@ -1,1 +1,5 @@
-from .quantizer import MixedPrecisionModelQuantizer
+# quantization/__init__.py
+from .quantizer import quantize_model
+from .utils import count_parameters, calculate_model_size, calculate_reduction_rate
+
+__all__ = ['quantize_model', 'count_parameters', 'calculate_model_size', 'calculate_reduction_rate']
