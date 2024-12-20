57b542482896f4c44e8409a679ff3cee004e0fc0
pierredantas <pierre.dantas@gmail.com>
2024-12-20 18:43:13 +0000

    Delete quantization/MixedPrecisionQuantization/src/mixed_precision directory

--- quantization/MixedPrecisionQuantization/src/mixed_precision/init.py
+++
@@ -1,4 +0,0 @@
-from .quantizer import quantize_model
-from .utils import count_parameters, calculate_model_size, calculate_reduction_rate
-
-__all__ = ['quantize_model', 'count_parameters', 'calculate_model_size', 'calculate_reduction_rate']
