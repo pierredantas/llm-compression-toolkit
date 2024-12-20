4727c61c7aa2d1e286835c44bbea0bfccdfc04c5
pierredantas <pierre.dantas@gmail.com>
2024-12-20 20:36:36 +0000

    Update mixed_precision_quantization.py

--- quantization/MixedPrecisionQuantization/mixed_precision_quantization.py
+++ quantization/MixedPrecisionQuantization/mixed_precision_quantization.py
@@ -1,6 +1,5 @@
 def quantize_bert_model(model_name: str = 'bert-base-uncased', save_path: str = 'bert-quantized'):
     import torch
-    import sys
     from transformers import BertModel
     from copy import deepcopy
     
